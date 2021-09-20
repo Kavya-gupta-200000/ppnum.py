aa5bb8410bdb86c44296a9a842ecf8f0493cfc00
Kavya-gupta-200000 <90207911+Kavya-gupta-200000@users.noreply.github.com>
2021-09-20 00:44:39 -0700

    developed by kavya

---
+++ ppnum.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 20 00:39:40 2021
+
+@author: kcs
+"""
+
+start, end = -4, 19
+#iterating each number in list
+for num in range(start, end + 1):
+    #checking condition
+    if num >= 0:
+        print(num, end = " ")
+
